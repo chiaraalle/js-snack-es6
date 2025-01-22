42bb4c8eb490ad04ee2e44efe3df4ccc8ae1b730
chiaralle <chiaraallegrino@gmail.com>
2025-01-22 16:18:54 +0100

    add: secondo snack

--- main.js
+++ main.js
@@ -41,8 +41,23 @@ Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
-Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console./*
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
 
+const squadre = [
+  { nome: 'Juventus', puntiFatti: 0, falliSubiti: 0 },
+  { nome: 'Inter', puntiFatti: 0, falliSubiti: 0 },
+  { nome: 'Milan', puntiFatti: 0, falliSubiti: 0 },
+  { nome: 'Roma', puntiFatti: 0, falliSubiti: 0 },
+  { nome: 'Napoli', puntiFatti: 0, falliSubiti: 0 }
+]; 
+
+for (let i = 0; i < squadre.length; i++) {
+     squadre[i].puntiFatti = Math.floor(Math.random() * 100) + 1;
+     squadre[i].falliSubiti = Math.floor(Math.random() * 100) + 1;
+
+  }
+
+console.log(squadre);
 
 /*Snack 3 (Bonus)
 Scrivere una funzione che accetti tre argomenti, un array ( esempio: ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'] )e due numeri (a più piccolo di b).
