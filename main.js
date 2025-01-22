79402e2576472021f3fd1fa3995e16fb0b3ca1df
chiaralle <chiaraallegrino@gmail.com>
2025-01-22 15:05:50 +0100

    add: ciclo for

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 /*Snack 1
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare a schermo la bici con peso minore.
+Stampare a schermo la bici con peso minore.*/
+const biciclette = [
+    {
+      nome: "Canyon Ultimate CF SLX",
+      peso: 6.8
+    },
+    {
+      nome: "Specialized S-Works Tarmac",
+      peso: 6.7
+    },
+    {
+      nome: "Trek Emonda SLR 9",
+      peso: 6.4
+    },
+    {
+      nome: "Bianchi Oltre XR4",
+      peso: 7.1
+    },
+    {
+      nome: "Pinarello Dogma F12",
+      peso: 6.9
+    }
+  ];
 
+  /*for (let i in biciclette) {
+    console.log(biciclette[i].peso);
+  }*/
+    for (let i = 0; i < biciclette.length; i++) {
+        console.log(biciclette[i].peso);
+      }
 
-Snack2
+/*Snack2
 Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
-Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console./*
 
 
-Snack 3 (Bonus)
+/*Snack 3 (Bonus)
 Scrivere una funzione che accetti tre argomenti, un array ( esempio: ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'] )e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con li elementi estratti dall'array di partenza che hanno la posizione compresa tra i due numeri (a e b).*/
